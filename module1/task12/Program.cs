bfd06e9b13d21bdaee728d91a39ccb2c7f48d336
Alexander Andreev <andreevlex.as@gmail.com>
2018-07-11 08:01:53 +0300

    решено 2 задачи модуля 1

---
+++ module1/task12/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace task12
+{
+    /*
+    12. Написать функцию нахождения максимального из трех чисел.
+    (c) Alexandex Andreev
+    */
+    class Program
+    {
+        static int GetNumber() {
+            System.Console.WriteLine("Введите число");
+            return Int32.Parse(Console.ReadLine());
+        }
+
+        static int Max(int num1, int num2) {
+            int result;
+
+            if(num1 > num2) {
+                result = num1;
+            }
+            else {
+                result = num2;
+            }
+
+            return result;
+        }
+
+        static int GetMaxFromNumbers(int num1, int num2, int num3) {
+            int max1 = Max(num1, num2);
+            return Max(max1, num3);
+        }
+
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Программа поиска максимума из 3-х чисел");
+            int num1 = GetNumber();
+            int num2 = GetNumber();
+            int num3 = GetNumber();
+
+            System.Console.WriteLine("Из чисел [{0}, {1}, {2}] максимальное {3}", num1, num2, num3, GetMaxFromNumbers(num1, num2, num3));
+        }
+    }
+}
---
+++ module1/task13/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace task13
+{
+    /*
+    13. * Написать функцию, генерирующую случайное число от 1 до 100.
+
+    с использованием стандартной функции rand()
+    без использования стандартной функции rand()
+    (c) Alexander Andreev
+    */
+    class Program
+    {
+        static int GetRandomNumber1() {
+            Random rnd = new Random();
+            return rnd.Next(0, 100);
+        }
+
+        static int Xn;
+        static int Modules = 100;
+        static void InitRandom(int m) {
+            Xn = DateTime.Now.Millisecond % m;
+        }
+
+        static int GetRandomNumber2() {
+            int a = 3;
+            int b = 7;
+            
+            Xn = (Xn * a + b) % Modules;
+            
+            return Xn;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Программа генерации случайных чисел");
+
+            System.Console.WriteLine("Использование стандартного объекта Random");
+            for(int i = 0; i < 20; i++) {
+                System.Console.WriteLine("{0}", GetRandomNumber1());
+            }
+
+            System.Console.WriteLine("Использование своего генератора");
+            InitRandom(Modules);
+            for (int i = 0; i < 20; i++)
+            {
+                System.Console.WriteLine("{0}", GetRandomNumber2());   
+            }
+        }
+    }
+}
