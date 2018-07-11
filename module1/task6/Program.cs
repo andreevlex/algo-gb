c1aa2cb4114e2e76f03eeddeace5906272c6af0b
Alexander Andreev <andreevlex.as@gmail.com>
2018-07-11 12:21:34 +0300

    решены 6 и 7 задачи моделя 1

---
+++ module1/task6/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace task6
+{
+    /*
+    6. Ввести возраст человека (от 1 до 150 лет) и вывести его вместе с последующим словом «год», «года» или «лет».
+    (c) Alexander Andreev
+    */
+    class Program
+    {
+        static int GetNumber(string suffix) {
+            System.Console.WriteLine($"Введите {suffix}");
+            return Int32.Parse(Console.ReadLine());
+        }
+
+        static string GetSuffix(int age)
+        {
+            if(age > 10 && age < 15)
+            {
+                return "лет";
+            }
+
+            switch (age % 10)
+            {
+                case 1:
+                return "год";
+                case 2:
+                case 3:
+                case 4:
+                return "года"; 
+                default:
+                return "лет";
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int age = GetNumber("возраст");
+
+            if(age < 0 || age > 150)
+            {
+                System.Console.WriteLine("Введен неверный возраст");
+            }
+            else
+            {
+                System.Console.WriteLine($"Человеку {age} {GetSuffix(age)}");       
+            }
+        }
+    }
+}
---
+++ module1/task7/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace task7
+{
+    /*
+    7. С клавиатуры вводятся числовые координаты двух полей шахматной доски (x1,y1,x2,y2).
+    Требуется определить, относятся ли к поля к одному цвету или нет.
+    (c) Alexander Andreev
+    */
+
+    public struct MyPoint
+    {
+        public int x;
+        public int y;
+    }
+
+    enum MyColor {
+        White = 0,
+        Black
+    }
+
+    class Program
+    {
+        static int GetNumber(string suffix) {
+            System.Console.WriteLine($"Введите {suffix}");
+            return Int32.Parse(Console.ReadLine());
+        }
+
+        static MyColor GetColor(MyPoint point)
+        {
+            if( (point.x + point.y) % 2 == 0 )
+            {
+                return MyColor.Black;
+            }
+            else
+            {
+                return MyColor.White;
+            }
+        }
+        static bool EqColor(MyPoint p1, MyPoint p2)
+        {
+            return GetColor(p1) == GetColor(p2);
+        }
+
+        static void Main(string[] args)
+        {
+            MyPoint p1 = new MyPoint();
+            MyPoint p2 = new MyPoint();
+  
+            p1.x = GetNumber("координату x1");
+            p1.y = GetNumber("координату y1");
+            System.Console.WriteLine("p1 цвета {0}", GetColor(p1));
+
+            p2.x = GetNumber("координату x2");
+            p2.y = GetNumber("координату y2");
+            System.Console.WriteLine("p2 цвета {0}", GetColor(p2));
+
+            if(EqColor(p1, p2))
+            {
+                System.Console.WriteLine("Клетки одинакового цвета");
+            }
+            else 
+            {
+                System.Console.WriteLine("Клетки разного цвета");           
+            }
+        }
+    }
+}
